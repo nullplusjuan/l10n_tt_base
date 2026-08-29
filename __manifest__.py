e9e81f0f3cfbad3ae44611c5a613af3471329346
Joshua Dhalai <joshuadhalai@live.com>
2026-08-29 01:40:37 -0400

    Add missing data file for Trinidad and Tobago localization

--- __manifest__.py
+++ __manifest__.py
@@ -35,6 +35,7 @@ statutory identifiers and tax rules should live in separate dependent modules.
     "depends": ["base", "partner_extended_profile"],
     "data": [
         "data/res_country_state_data.xml",
+        "data/res_country_data.xml",
     ],
     "installable": True,
     "application": False,
